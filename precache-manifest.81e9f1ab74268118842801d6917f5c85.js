3149add7f8cfb487e35d7a6e04ab1874b4220535
Vin EEth P <vineeth.pulikote@rove-india.com>
2019-10-12 11:35:53 +0530

    Updates

---
+++ precache-manifest.81e9f1ab74268118842801d6917f5c85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8848fcbb09aba195af1186e9da724a",
+    "url": "/infyassgn/index.html"
+  },
+  {
+    "revision": "2b09672743c465087e8c",
+    "url": "/infyassgn/static/css/main.85121d80.chunk.css"
+  },
+  {
+    "revision": "38e65ad584618296744c",
+    "url": "/infyassgn/static/js/2.834e90e2.chunk.js"
+  },
+  {
+    "revision": "2b09672743c465087e8c",
+    "url": "/infyassgn/static/js/main.1f123c17.chunk.js"
+  },
+  {
+    "revision": "9dbb733e97232140ede2",
+    "url": "/infyassgn/static/js/runtime-main.4b502b40.js"
+  }
+]);
