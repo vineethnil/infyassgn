c9e3848a9ca1d1d3a2a1de7e2987170029b9d9e8
Vin EEth P <vineeth.pulikote@rove-india.com>
2019-10-12 11:22:02 +0530

    making it better

---
+++ build/precache-manifest.bdca4d57154d753a28e31b70f542bd37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47aeddcfb7d57edd50e019ab4dde3364",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ca147c40311b475f7f4",
+    "url": "/static/css/main.85121d80.chunk.css"
+  },
+  {
+    "revision": "38e65ad584618296744c",
+    "url": "/static/js/2.834e90e2.chunk.js"
+  },
+  {
+    "revision": "5ca147c40311b475f7f4",
+    "url": "/static/js/main.fd865bd0.chunk.js"
+  },
+  {
+    "revision": "b60dcbf12de12224c63e",
+    "url": "/static/js/runtime-main.be23f2bf.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route,Switch } from "react-router-dom";
 import './App.css';
+import ScreenOne from "./components/ScreenOne";
+import Upload from "./components/Upload";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = { 
+        screenonedata:[
+          {
+            id:"data1",
+            label:"Name",
+            type:"text",
+            field:"input",
+            value:"John",
+          },
+          {
+            id:"data2",
+            label:"ID",
+            type:"number",
+            field:"input",
+            value:"12",
+          },
+          {
+            id:"data3",
+            label:"DOB",
+            type:"date",
+            field:"input",
+            value:"2019-12-10",
+          }
+        ],
+        screentwodata:[
+          {
+            id:"data1",
+            label:"Company Id",
+            type:"text",
+            field:"input",
+            value:"12",
+          },
+          {
+            id:"data2",
+            label:"Company Name",
+            type:"Text",
+            field:"input",
+            value:"RBS",
+          },
+          {
+            id:"data3",
+            label:"Comment",
+            type:"text",
+            field:"textarea",
+            value:"This is a sample comment",
+          },
+          {
+            id:"data4",
+            label:"Rating",
+            field:"select",
+            option:[5,4,3,2,1],
+            value:"1",
+          },
+        ],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="main_outer_cntr">
+          <Switch>
+            <Route path="/" exact render={(props) => <ScreenOne {...props} tableprops={this.state.screenonedata} />}/>
+            <Route path="/screen1" exact render={(props) => <ScreenOne {...props} tableprops={this.state.screenonedata} />}/>
+            <Route path="/screen2" exact render={(props) => <ScreenOne {...props} tableprops={this.state.screentwodata} />}/>
+            <Route path="/upload" component={Upload}/>
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+
